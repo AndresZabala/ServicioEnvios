4e6446f4df454a996c9af85da4258fb51c87d67c
Andres Zabala <andrezabala138@gmail.com>
2020-04-03 21:11:44 -0500

    Creacion Proyecto Servicio Envios WCF

---
+++ ServiciosEnvios/Utilidades/Util.cs
@@ -0,0 +1,19 @@
+﻿using ServiciosEnvios.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiciosEnvios.Utilidades
+{
+    public class Util
+    {
+        
+
+        public bool encontroCliente(string cedula, enviosEntidades bd)
+        {
+            CLIENTE efCliente = bd.CLIENTE.Find(cedula);
+            return (efCliente != null);
+        }
+    }
+}
